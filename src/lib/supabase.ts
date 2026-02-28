6a8e2cc9f5c40b10d951f324ddd508814d5b97a0
ShivenduShivu <shivendukumarbadal328@gmail.com>
2026-03-01 00:10:00 +0530

    updated supabase

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,8 +1,9 @@
-import { createClient } from "@supabase/supabase-js"
+import { createBrowserClient } from "@supabase/ssr"
 
-// Env variables from .env.local
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
-// Create Supabase client
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createBrowserClient(
+  supabaseUrl,
+  supabaseAnonKey
+)
